f31b970b23243b764ce696c2e36830529e166a77
Jay Kadam <jaykad982@gmail.com>
2026-03-01 01:30:59 -0500

    added main script and settings script

---
+++ Assets/HierarchyGarnish/HierarchyGarnish.cs
@@ -0,0 +1,17 @@
+using UnityEditor;
+using UnityEngine;
+
+
+
+namespace HierarchyGarnish
+{
+    [InitializeOnLoad]
+    public static class HierarchyGarnish
+    {
+        //cache guis 
+        private static GUIStyle HeaderStyle;
+        private static GUIStyle BadgeStyle;
+    }
+}
+
+
